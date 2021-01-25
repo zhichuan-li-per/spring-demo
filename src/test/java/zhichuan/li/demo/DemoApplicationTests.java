3c98202ea446f4a779c09b597bd80161c486f196
zhichuan-li-per <zhichuan.li1988@gmail.com>
2021-01-25 20:03:49 +0100

    First commit

---
+++ src/test/java/zhichuan/li/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package zhichuan.li.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
